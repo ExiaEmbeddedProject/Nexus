d13a08cce318e807575ff1f01a219cc6c62941fb
Cprice <cprice@tutanota.com>
2016-06-20 10:21:49 +0200

    Write in file

--- nexus/nexus.ino
+++ nexus/nexus.ino
@@ -1,21 +1,26 @@
 // Humidity sensor
-#include "DHT.h"
 // Gyro
-#include<Wire.h>
+
+#include <Wire.h>
+
+#include <SPI.h>
+#include <SD.h>
+
+#include "DHT.h"
 // GPS
 #include "TinyGPS++.h"
 #include <SoftwareSerial.h>
 
 
-#define HUMIDITY_PIN 4
+#define HUMIDITY_PIN 2
 #define HUMIDITY_TYPE DHT22
 #define MPU 0x68
 #define MAX_FILE_ENTRIES 3000
-#define GPS_BAUD 4800
+#define GPS_BAUD 9600
 #define GPS_RXP_IN 4
 #define GPS_TXP_IN 3
 #define FREQUENCY 0.2 // Hz
-
+#define SD_PIN 10
 
 // Humidity sensor
 DHT dht(HUMIDITY_PIN, HUMIDITY_TYPE);
@@ -23,100 +28,116 @@ DHT dht(HUMIDITY_PIN, HUMIDITY_TYPE);
 TinyGPSPlus gps;
 SoftwareSerial ss(GPS_RXP_IN, GPS_TXP_IN);
 
-
-long id = 0;
-int lineNumber = 0;
-int fileNumber = 1;
+unsigned long id = 0;
+unsigned int lineNumber = 0;
+unsigned int fileNumber = 1;
+unsigned int humidity = 0;
 char fileName[50];
-char json[200];
+char json[40];
 
-int humidity;
-float temperature;
 float gyro[3];
 float accel[3];
-float latitude;
-float longitude;
-float speed; // km/h
+float latitude = 0;
+float longitude = 0;
+float temperature = 0;
 TinyGPSDate date;
 TinyGPSTime time;
 
-
-void updateJson() {
-  sprintf(json, "%s", "{\"DATA\":[]}");
+void writeInitialStructure() {
+  writeInFile(F("{\"DATA\":[]}"), false, false);
 }
 
-void printInfos() {
-  Serial.println("------------------------");
-  
-  Serial.println("----FILE----");
-  
-  Serial.print("Id: ");
-  Serial.println(id);
-  Serial.print("LineNumber: ");
-  Serial.println(lineNumber);
-  Serial.print("FileNumber: ");
-  Serial.println(fileNumber);
-  Serial.print("FileName: ");
-  Serial.println(fileName);
-  
-  Serial.println("---SENSOR---");
-  
-  Serial.print("Date: ");
-  Serial.print(date.year(), DEC);
-  Serial.print('/');
-  Serial.print(date.month(), DEC);
-  Serial.print('/');
-  Serial.print(date.day(), DEC);
-  Serial.print(' ');
-  Serial.print(time.hour(), DEC);
-  Serial.print(':');
-  Serial.print(time.minute(), DEC);
-  Serial.print(':');
-  Serial.print(time.second(), DEC);
-  Serial.print('.');
-  Serial.print(time.centisecond(), DEC);
-  Serial.println();
-  
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.println("%");
-  
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.println("°c");
-  
-  Serial.println("Gyro: ");
-  Serial.print("   x: ");
-  Serial.println(gyro[0]);
-  Serial.print("   y: ");
-  Serial.println(gyro[1]);
-  Serial.print("   z: ");
-  Serial.println(gyro[2]);
-  
-  Serial.println("Acceleration: ");
-  Serial.print("   x: ");
-  Serial.println(accel[0]);
-  Serial.print("   y: ");
-  Serial.println(accel[1]);
-  Serial.print("   z: ");
-  Serial.println(accel[2]);
+void writeData() {
+  writeInFile(F("{\"id\":\""), true, true);
+  writeInFile((String)(id), true, false);
 
-  Serial.println("GPS: ");
-  Serial.print("   lat: ");
-  Serial.println(latitude);
-  Serial.print("   lng: ");
-  Serial.println(longitude);
-  Serial.print("   speed: ");
-  Serial.print(speed);
-  Serial.println("km/h");
+  writeInFile(F("\",\"DateHeure\":"), true, false);
+  writeInFile((String)(date.year()), true, false);
+  writeInFile(F("-"), true, false);
+  writeInFile((String)(date.month()), true, false);
+  writeInFile(F("-"), true, false);
+  writeInFile((String)(date.day()), true, false);
+  writeInFile(F("T"), true, false);
+  writeInFile((String)(time.hour()), true, false);
+  writeInFile(F(":"), true, false);
+  writeInFile((String)(time.minute()), true, false);
+  writeInFile(F(":"), true, false);
+  writeInFile((String)(time.second()), true, false);
+  writeInFile(F(":"), true, false);
+  writeInFile((String)(time.centisecond()), true, false);
+  writeInFile(F("Z"), true, false);
+  
+  
+  writeInFile(F("\",\"Humidite\":"), true, false);
+  writeInFile((String)(humidity), true, false);
+
+  writeInFile(F(",\"Temperature\":"), true, false);
+  writeInFile((String)(temperature), true, false);
+
+  writeInFile(F(",\"Accel_x\":"), true, false);
+  writeInFile((String)(accel[0]), true, false);
+
+  writeInFile(F(",\"Accel_y\":"), true, false);
+  writeInFile((String)(accel[1]), true, false);
+
+  writeInFile(F(",\"Accel_z\":"), true, false);
+  writeInFile((String)(accel[2]), true, false);
+
+  writeInFile(F(",\"Gyro_yaw\":"), true, false);
+  writeInFile((String)(gyro[0]), true, false);
+
+  writeInFile(F(",\"Gyro_pitch\":"), true, false);
+  writeInFile((String)(gyro[1]), true, false);
+
+  writeInFile(F(",\"Gyro_roll\":"), true, false);
+  writeInFile((String)(gyro[2]), true, false);
+
+  writeInFile(F(",\"Latitude\":"), true, false);
+  writeInFile((String)(latitude), true, false);
+
+  writeInFile(F(",\"Longitude\":"), true, false);
+  writeInFile((String)(longitude), true, false);
+
+  writeInFile(F("}]}"), true, false);
+  
+  //sprintf(json, "{\"id\":\"%d\"", id);
+  //writeInFile(json, true, true);
+  /*sprintf(json, ",\"Humidite\":%d", humidity);
+  writeInFile(json, true, false);
+  char str_temp[6];
+  dtostrf(temperature, 3, 2, str_temp);
+  sprintf(json, ",\"Temperature\":%s", str_temp);
+  writeInFile(json, true, false);
+  dtostrf(accel[0], 2, 2, str_temp);
+  sprintf(json, ",\"Accel_x\":%s}]}", str_temp);
+  writeInFile(json, true, false);
+  sprintf(json, ",\"Accel_y\":%s}]}", str_temp);
+  writeInFile(json, true, false);
+  sprintf(json, ",\"Accel_z\":%s}]}", str_temp);
+  writeInFile(json, true, false);
+  */
 }
 
-void writeInFile() {
-  
-}
-
-void initHumidity() {
-  dht.begin();
+void writeInFile(String data, bool inject, bool newObject) {
+  File dataFile = SD.open(fileName, FILE_WRITE);
+  if (dataFile) {
+    if (inject) {
+      if(newObject) {
+        dataFile.seek(dataFile.size() - 2 * sizeof(char));
+      }
+      else {
+        dataFile.seek(dataFile.size());
+      }
+    }
+    if (lineNumber != 1 && newObject)
+      dataFile.print(F(","));
+    dataFile.print(data);
+    dataFile.close();
+  }
+  else
+  {
+    //Serial.println(F("Failed to access to file."));
+  }
 }
 
 void initGyro() {
@@ -127,18 +148,17 @@ void initGyro() {
   Wire.endTransmission(true);
 }
 
-void initGps() {
-  ss.begin(GPS_BAUD);
-}
-
 void update() {
   id++;
   lineNumber++;
+  if (lineNumber == MAX_FILE_ENTRIES || id == 1) {
+    sprintf(fileName, "%s%06d%s", "04", fileNumber, ".txt");
+    writeInitialStructure();
+  }
   if (lineNumber == MAX_FILE_ENTRIES) {
     fileNumber++;
     lineNumber = 0;
   }
-  if (lineNumber == MAX_FILE_ENTRIES || id == 1) sprintf(fileName, "%s%06d%s", "04", fileNumber, ".txt");
 }
 
 void updateHumidity() {
@@ -149,21 +169,20 @@ void updateGyro() {
   Wire.beginTransmission(MPU);
   Wire.write(0x3B);
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU,14,true);
-  accel[0] = Wire.read()<<8|Wire.read();  // (ACCEL_XOUT_L)    
-  accel[1] = Wire.read()<<8|Wire.read();  // (ACCEL_YOUT_L)
-  accel[2] = Wire.read()<<8|Wire.read();  // (ACCEL_ZOUT_L)
-  temperature = (Wire.read()<<8|Wire.read()) / 340.00 + 36.53;  // (TEMP_OUT_L)
-  gyro[0] = Wire.read()<<8|Wire.read();  // (GYRO_XOUT_L)
-  gyro[1] = Wire.read()<<8|Wire.read();  // (GYRO_YOUT_L)
-  gyro[2] = Wire.read()<<8|Wire.read();  // (GYRO_ZOUT_L)
+  Wire.requestFrom(MPU, 14, true);
+  accel[0] = (Wire.read() << 8 | Wire.read()) / 16384 * 9.80665; // (ACCEL_XOUT_L)
+  accel[1] = (Wire.read() << 8 | Wire.read()) / 16384 * 9.80665; // (ACCEL_YOUT_L)
+  accel[2] = (Wire.read() << 8 | Wire.read()) / 16384 * 9.80665; // (ACCEL_ZOUT_L)
+  temperature = (Wire.read() << 8 | Wire.read()) / 340.00 + 36.53 + 273.15; // (TEMP_OUT_L)
+  gyro[0] = (Wire.read() << 8 | Wire.read()) / 131; // (GYRO_XOUT_L)
+  gyro[1] = (Wire.read() << 8 | Wire.read()) / 131; // (GYRO_YOUT_L)
+  gyro[2] = (Wire.read() << 8 | Wire.read()) / 131; // (GYRO_ZOUT_L)
 }
 
 void updateGps() {
-  if(gps.location.isValid()) {
+  if (gps.location.isValid()) {
     latitude = gps.location.lat();
     longitude = gps.location.lng();
-    speed = gps.speed.kmph();
     date = gps.date;
     time = gps.time;
   }
@@ -171,28 +190,30 @@ void updateGps() {
 
 void setup() {
   Serial.begin(9600);
-  initHumidity();
+  SD.begin(SD_PIN);
+  dht.begin();
+  ss.begin(GPS_BAUD);
   initGyro();
-  initGps();
-  Serial.println("Initialized");
+  Serial.println(F("K"));
 }
+
+
 
 void loop() {
   update();
   updateHumidity();
   updateGyro();
   updateGps();
-  printInfos();
-  writeInFile();
-  smartDelay(1/FREQUENCY*1000);
-  if (millis() > 5000 && gps.charsProcessed() < 10)
-    Serial.println(F("/!\\ No GPS data received, check wiring /!\\"));
+  writeData();
+  smartDelay(1 / FREQUENCY * 1000);
+  //if (millis() > 5000 && gps.charsProcessed() < 10)
+    //Serial.println(F("/!\\ No GPS data received, check wiring /!\\"));
 }
 
 static void smartDelay(unsigned long ms)
 {
   unsigned long start = millis();
-  do 
+  do
   {
     while (ss.available())
       gps.encode(ss.read());
